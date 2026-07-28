92ccdc192635e17fc04fde3b21b66ed4dbb44e4d
unknown <AaryanBhende>
2026-07-28 15:17:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Register from "./Register.jsx"
+import Login from "./Login.jsx"
+import Home from "./Home.jsx"
+function App() {
+
+    return(
+        // <Register></Register>
+        <Login></Login>
+        // <Home></Home>
+    )
+
+}
+
+export default App
